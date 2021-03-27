a4254dcab0daedfb7eb6571773de1206ee2f4f53
Ewen Le Bihan <hey@ewen.works>
2021-03-27 18:39:27 +0100

    polybar: music module: use playerctl, dynamically get network interface

--- .config/polybar/run_polybar.sh
+++ .config/polybar/run_polybar.sh
@@ -6,12 +6,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Determine network interface based on hostname (might need a better way to get it though)
+# Determine network interface 
+adaptater=`ip link | grep 'state UP' | cut -d':' -f2 | cut -d' ' -f2`
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 for monitor in $(xrandr --query | grep ' connected' | cut -d' ' -f1); do
-	MONITOR=$monitor polybar -c ~/.config/polybar/config.ini top &
-	MONITOR=$monitor polybar -c ~/.config/polybar/config.ini bottom &
+	ADAPTATER=$adaptater MONITOR=$monitor polybar -c ~/.config/polybar/config.ini top &
+	ADAPTATER=$adaptater MONITOR=$monitor polybar -c ~/.config/polybar/config.ini bottom &
 done
 
 echo "Polybar launched."
