8ebf92d9aa1e63bb3dfc9808f8d091ba89542913
Ewen Le Bihan <hey@ewen.works>
2021-03-29 17:07:11 +0200

    polybar: fix run script
    rename variable (adaptater -> interface)
    
    get first line only (in case multiple interfaces are mode DORMANT)

--- .config/polybar/run_polybar.sh
+++ .config/polybar/run_polybar.sh
@@ -7,12 +7,13 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Determine network interface 
-adaptater=`ip link | grep 'mode DORMANT' | cut -d':' -f2 | cut -d' ' -f2`
+interface=`ip link | grep 'mode DORMANT' | head -n1 | cut -d':' -f2 | cut -d' ' -f2`
+echo "Using interface $interface"
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 for monitor in $(xrandr --query | grep ' connected' | cut -d' ' -f1); do
-	ADAPTATER=$adaptater MONITOR=$monitor polybar -c ~/.config/polybar/config.ini top &
-	ADAPTATER=$adaptater MONITOR=$monitor polybar -c ~/.config/polybar/config.ini bottom &
+	INTERFACE=$interface MONITOR=$monitor polybar -l=notice -c ~/.config/polybar/config.ini top &
+	INTERFACE=$interface MONITOR=$monitor polybar -c ~/.config/polybar/config.ini bottom &
 done
 
 echo "Polybar launched."
