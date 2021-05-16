18a99f0e1625f0f5d34a49cb7b90c37e56b77983
Ewen Le Bihan <hey@ewen.works>
2021-05-16 17:48:12 +0200

    ideaseed: fix default-user-{project,column} swapped around

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -70,4 +70,4 @@ end
 alias lynx "lynx --display_charset=utf-8"
 
 thefuck --alias | source
-alias idea='ideaseed --auth-cache=/home/ewen/.cache/ideaseed/auth.json --check-for-updates --self-assign --create-missing --local-copy=/home/ewen/ideas --default-project=\'{repository}\' --default-column=todo --default-user-column=incubator --default-user-project=willmake'
+alias idea='ideaseed --auth-cache=/home/ewen/.cache/ideaseed/auth.json --check-for-updates --self-assign --create-missing --local-copy=/home/ewen/ideas --default-project=\'{repository}\' --default-column=todo --default-user-column=willmake --default-user-project=incubator'
