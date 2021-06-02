fec7db322f022b985183fa7e8549a522693fcb9d
Ewen Le Bihan <hey@ewen.works>
2021-06-02 22:57:23 +0200

    add fish abbreviations
    needs manual updating for now

---
+++ abbreviations.fish
@@ -0,0 +1,65 @@
+abbr -a -U -- :q exit
+abbr -a -U -- :x exit
+abbr -a -U -- à 'idea rocketbook àrevoir thermodynamique "'
+abbr -a -U -- àcf 'idea rocketbook àrevoir champs-forces'
+abbr -a -U -- àrevoir 'idea rocketbook àrevoir'
+abbr -a -U -- àtherm 'idea rocketbook àrevoir thermodynamique'
+abbr -a -U -- é /home/ewen
+abbr -a -U -- c clear
+abbr -a -U -- cleanlatexdir 'rm *.{aux,fdb_latexmk,log,out,synctex.gz,fls,toc}'
+abbr -a -U -- codestable code
+abbr -a -U -- dark 'switch-color-scheme dark'
+abbr -a -U -- e 'neovide --multiGrid'
+abbr -a -U -- executable 'chmod +x'
+abbr -a -U -- fu fuck
+abbr -a -U -- gc 'git commit -m'
+abbr -a -U -- gj 'gitmoji -c'
+abbr -a -U -- grep rg
+abbr -a -U -- grepp grep
+abbr -a -U -- icat 'kitty +kitten icat'
+abbr -a -U -- ideadot 'idea -R dotfiles'
+abbr -a -U -- ideaidx 'idea schoolsyst/indentex -i'
+abbr -a -U -- ideap 'idea portfolio -iT"'
+abbr -a -U -- idxheaders rg\ -N\ \'\^\[\~=-\]\{3,4\}\\s\'
+abbr -a -U -- indentexnew rg\ -A\ 4\ \'\(NEW\|TODO\)!\'\ \~/\*\*.indentex
+abbr -a -U -- j z
+abbr -a -U -- khôlles-next-dates 'cat ~/.cache/khôlles-schedules/*.tsv | sort | head -n2'
+abbr -a -U -- la 'lsd -A'
+abbr -a -U -- lampp 'doas /opt/lampp/lampp restart && qutebrowser localhost/phpmyadmin'
+abbr -a -U -- le 'lsd --sort extension'
+abbr -a -U -- lg lazygit
+abbr -a -U -- light 'switch-color-scheme light'
+abbr -a -U -- ll 'lsd -lA'
+abbr -a -U -- lls ll
+abbr -a -U -- ls lsd
+abbr -a -U -- lss ls
+abbr -a -U -- make-local-py-env-symlink 'ln -s (poetry env info --path)/bin .interpreter'
+abbr -a -U -- myip 'dig +short myip.opendns.com @resolver1.opendns.com'
+abbr -a -U -- orca /home/ewen/apps/Orca-c/build/orca
+abbr -a -U -- p python
+abbr -a -U -- pdfa4 'pdftocairo -pdf -paper A4 -expand'
+abbr -a -U -- plattan2 'bluetoothctl connect 5C:EB:68:4A:3E:CA'
+abbr -a -U -- qute qutebrowser
+abbr -a -U -- r ranger
+abbr -a -U -- rm 'rm -I'
+abbr -a -U -- rmdir rmdirtrash
+abbr -a -U -- rrm 'rm -I'
+abbr -a -U -- savealias 'echo alias = '
+abbr -a -U -- scim sc-im
+abbr -a -U -- sls ls
+abbr -a -U -- ssudo sudo
+abbr -a -U -- synctime 'sudo ntpd -qg'
+abbr -a -U -- timesync 'sudo ntpd -qg'
+abbr -a -U -- tlmgr tllocalmgr
+abbr -a -U -- tree 'lsd --tree'
+abbr -a -U -- ts 'sudo ntpd -qg'
+abbr -a -U -- unstage 'git reset'
+abbr -a -U -- use-pacman-stable-repo 'doas pacman-mirrors -aB stable && doas pacman -Syuu'
+abbr -a -U -- use-pacman-unstable-repo 'doas pacman-mirrors -aB unstable && doas pacman -Syuu'
+abbr -a -U -- wa tungsten
+abbr -a -U -- whats 'tuxi what is'
+abbr -a -U -- whereami 'http get ip-api.com/json/(dig +short myip.opendns.com @resolver1.opendns.com) --body | jq'
+abbr -a -U -- wttr 'curl wttr.in'
+abbr -a -U -- xclip 'xclip -selection clipboard'
+abbr -a -U -- yay paru
+abbr -a -U -- ytdl youtube-dl
