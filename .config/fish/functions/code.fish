978fc1364fb98adbfac9d4a1513a42e01e6be289
Ewen Le Bihan <hey@ewen.works>
2021-06-15 06:16:44 +0200

    functions: remove code

--- .config/fish/functions/code.fish
+++
@@ -1,7 +0,0 @@
-function code --description "Clone with gclone if the given first argument is not an existing file (directory or not)"
-    set --local name "$argv[1]"
-    if not test -e $name
-        gclone $name
-    end
-    code-insiders $argv
-end
