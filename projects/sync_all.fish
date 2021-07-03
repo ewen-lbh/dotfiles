8dd3f474ddd1ad0fcfd8d6be35d79b1916f16151
Ewen Le Bihan <hey@ewen.works>
2021-07-03 16:06:43 +0200

    projects/sync_all: run all concurrently

--- projects/sync_all.fish
+++ projects/sync_all.fish
@@ -3,41 +3,19 @@
 # vim: set shiftwidth=2 :
 # vim: set noexpandtab :
 
-function _git_is_ahead
-	set ahead_or_behind (git status -uno | rg -o 'Your branch is (behind|ahead)' -r '$1') 
-	test "$ahead_or_behind" = "ahead"
-end
-
-function _git_is_behind
-	set ahead_or_behind (git status -uno | rg -o 'Your branch is (behind|ahead)' -r '$1') 
-	test "$ahead_or_behind" = "behind"
-end
-
-set -l separator "-----------------------------------------------------------------------------"
 set -l initialwd (pwd)
+set -l commands ()
+set -l names ""
 
 for d in (status dirname)/*
 	if test -d $d
 		cd $d
 		if test -d .git 
-			set_color --dim
-			echo "$d: fetching"
-			set_color normal
-			git fetch
-			if _git_is_behind
-				set_color magenta --reverse --bold 
-				echo "$d: pulling"
-				set_color normal
-				git pull
-				echo $separator
-			else if _git_is_ahead
-				set_color magenta --reverse --bold 
-				echo "$d: pushing"
-				set_color normal
-				git push
-				echo $separator
-			end
+			set commands $commands "cd $d; git fetch; git pull; git push"
+			set names "$names,"(basename $d)
 		end
 		cd "$initialwd"
 	end
 end
+
+concurrently $commands --names $names
