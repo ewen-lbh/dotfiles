55800b3278b656cc8ef050d5abf6a63c78b98170
Ewen Le Bihan <hey@ewen.works>
2021-07-05 17:53:06 +0200

    ranger: try using glow to preview markdown

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -94,6 +94,10 @@ case "$extension" in
         try lynx   -dump "$path" && { dump | trim | fmt -s -w $width; exit 4; }
         try elinks -dump "$path" && { dump | trim | fmt -s -w $width; exit 4; }
         ;; # fall back to highlight/cat if the text browsers fail
+    # Markdown documents:
+    md|markdown|mdown)
+        try glow "$path" && { dump | trim | fmt -s -w $width; exit 4};;
+    
 esac
 
 case "$mimetype" in
