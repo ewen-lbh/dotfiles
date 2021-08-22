4ad72bffd55ed82655ed6c00eb5dfc9cf1384fbe
Ewen Le Bihan <hey@ewen.works>
2021-08-23 00:15:54 +0200

    polybar: spotify: add remaining time

--- .config/polybar/run_polybar.sh
+++ .config/polybar/run_polybar.sh
@@ -30,7 +30,7 @@ color=$(grep light $HOME/.config/current_color_scheme 1>/dev/null && echo \#000 
 # Launch spotify metadata receiver script
 # TODO: kill previous instances within spotify-metadata-receiver.py itself
 kill $(pgrep -af "python $HOME/.config/polybar/spotify-metadata-receiver.py" | cut -d' ' -f1)
-python ~/.config/polybar/spotify-metadata-receiver.py localhost 8887 '{elapsed/duration*100:2.0f}% {i("♥ ", liked)}{i("¹↷", loop)}{i("⇄", shuffle)}{artist} — {title}' ~/.config/polybar/spotify-metadata.txt &
+python ~/.config/polybar/spotify-metadata-receiver.py localhost 8887 '{i("♥ ", liked)}{i("¹↷", loop)}{i("⇄", shuffle)} {artist} — {title} -{chr(39).join(map("{:02}".format, divmod(duration-elapsed, 60)))}"' ~/.config/polybar/spotify-metadata.txt 2>receiver_errors.txt &
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 for monitor in $(xrandr --query | grep ' connected' | cut -d' ' -f1); do
