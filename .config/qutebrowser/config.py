5919ff024dd26f24f52e9d2d006c1e9843aa6fc3
Ewen Le Bihan <hey@ewen.works>
2021-11-02 22:54:09 +0100

    qute: change UI font to Victor Mono

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -230,14 +230,14 @@ c.colors.webpage.darkmode.policy.page = 'always'
 # font setting, it's replaced with the fonts listed here. If set to an
 # empty value, a system-specific monospace default is used.
 # Type: List of Font, or Font
-c.fonts.default_family = 'scientifica'
+c.fonts.default_family = "Victor Mono"
 
 # Default font size to use. Whenever "default_size" is used in a font
 # setting, it's replaced with the size listed here. Valid values are
 # either a float value with a "pt" suffix, or an integer value with a
 # "px" suffix.
 # Type: String
-c.fonts.default_size = '11.5pt'
+c.fonts.default_size = "9.5pt"
 
 # Font used for the hints.
 # Type: Font
