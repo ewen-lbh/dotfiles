62b59ca675974018267f77c8602b41732650f7f1
Ewen Le Bihan <hey@ewen.works>
2023-11-03 18:13:50 +0100

    2023-11-03T18:13:50+01:00

--- .config/fish/functions/colorswitch.fish
+++ .config/fish/functions/colorswitch.fish
@@ -1,16 +1,28 @@
+function edit-json
+	jq $argv[2] $argv[1] | sponge $argv[1]
+end
+
 function colorswitch
 	if test $argv[1] = "dark"
 		set variant mocha
 		set Variant Mocha
 		set DarkOrLight Dark
+		set darkOrLight dark
 	else
 		set variant latte
 		set Variant Latte
 		set DarkOrLight Light
+		set darkOrLight light
 	end
 
 	# Others
 	echo $argv[1] > ~/.config/colorscheme
+
+	# Wallpaper (hyprpaper)
+	sed -i "s|^wallpaper = .*\$|wallpaper = eDP-1,$HOME/.config/wallpaper-$darkOrLight.png|" $HOME/.config/hypr/hyprpaper.conf
+	killall hyprpaper
+	hyprpaper &>/dev/null & disown
+
 
 	# Kitty
 	kitten themes --reload-in=all "Catppuccin-$variant"
@@ -24,10 +36,10 @@ function colorswitch
 	set -Ux LG_CONFIG_FILE "$lazygit_config_dir/config.yml,$lazygit_config_dir/$variant.yml"
 
 	# Dunst
-	killall dunst 2>/dev/null
+	killall dunst &>/dev/null
 	set dunst_config_dir "$HOME/.config/dunst"
 	cat $dunst_config_dir/base.conf $dunst_config_dir/$variant.conf > $dunst_config_dir/dunstrc
-	dunst & disown
+	dunst &>/dev/null & disown
 
 	# Bat
 	set -ge BAT_THEME
@@ -35,12 +47,15 @@ function colorswitch
 
 	# VS Code
 	set vscode_settings "$HOME/.config/Code/User/settings.json"
-	yq -i $vscode_settings ".workbench.colorTheme = .\"workbench.preferred$(echo $DarkOrLight)ColorTheme\"" 
-	yq -i $vscode_settings ".workbench.iconTheme = \"catppuccin-$variant\"" 
+	edit-json $vscode_settings \
+		".workbench.colorTheme = .\"workbench.preferred$(echo $DarkOrLight)ColorTheme\"" $vscode_settings
+	edit-json $vscode_settings \
+		".workbench.iconTheme = \"catppuccin-$variant\""  
 
 	# (Better)Discord
 	set betterdiscord_themes_config "$HOME/.config/BetterDiscord/data/stable/themes.json"
-	yq -i $betterdiscord_themes_config "map_values(false) | .\"Catppuccin $Variant\" = true" 
-	betterdiscordctl reinstall
-	discord &
+	edit-json $betterdiscord_themes_config \
+		"map_values(false) | .\"Catppuccin $Variant\" = true" 
+	betterdiscordctl &>/dev/null reinstall
+	discord &>/dev/null & disown
 end
