0581511fc0b6cb978cfe6c5435fd6719286707c7
Ewen Le Bihan <hey@ewen.works>
2024-08-21 13:21:33 +0200

    up

--- .config/fish/functions/autoadb.fish
+++ .config/fish/functions/autoadb.fish
@@ -1,4 +1,4 @@
 function autoadb
-set port (nmap 192.168.1.24 -p 37000-44000 -oG - | grep -P '\d+(?=\/open\/tcp)' --only-matching)
+set port (nmap 192.168.1.24 -p 37000-45000 -oG - | grep -P '\d+(?=\/open\/tcp)' --only-matching)
 adb connect 192.168.1.24:$port
 end
